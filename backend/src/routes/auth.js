e06e1b5134e0fe2a1a92cbd4231363ae7f670c18
ddodxy <alfariziridha014@gmail.com>
2026-02-05 01:58:17 -0300

    .

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,7 +1,7 @@
 ﻿const express = require("express");
 const router = express.Router();
 const db = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const { v4: uuidv4 } = require("uuid");
 
